3d0d8f6a7eb17c8cbcd5c02fd95b74dd8356a2aa
Udita Nandi-AI <nandigugu2006@gmail.com>
2025-07-14 01:01:35 +0530

    done

--- strings.c
+++ strings.c
@@ -289,3 +289,28 @@ void checkchar(char str[], char ch)
 
     printf("character is not present");
 }
+
+// occurence of vowels in a strings using function//
+#include <stdio.h>
+#include <string.h>
+int countVowels(char str[]);
+int main()
+{
+    char str[] = "helloworld";
+
+    printf("vowels are :%d", countVowels(str));
+}
+int countVowels(char str[])
+{
+
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
